7fab1269c93686fa2364e68798b4cf465e452eef
Imran Hossen <freelancerimran81@gmail.com>
2022-06-30 00:53:29 +0600

    updated

--- Server/utils/verifyToken.js
+++ Server/utils/verifyToken.js
@@ -1,18 +1,18 @@
 import jwt from "jsonwebtoken";
-import { createError } from '../utils/error.js'
+import { createError } from "../utils/error.js";
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
     if (!token) {
-        return next(createError(401, "You are not authenticated!"))
+        return next(createError(401, "You are not authenticated!"));
     }
 
     jwt.verify(token, process.env.JWT, (err, user) => {
-        if (err) return next(createError(403, "Token is not valid!"))
+        if (err) return next(createError(403, "Token is not valid!"));
         req.user = user;
-        next()
-    })
-}
+        next();
+    });
+};
 
 export const verifyUser = (req, res, next) => {
     verifyToken(req, res, next, () => {
