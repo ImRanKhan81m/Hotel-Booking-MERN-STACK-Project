457afb1d51052b1be589c3021e4fd0674891367c
Imran Hossen <freelancerimran81@gmail.com>
2022-07-07 00:59:38 +0600

    updated

--- Server/routes/rooms.js
+++ Server/routes/rooms.js
@@ -1,7 +1,21 @@
 import express from "express"
+import { createRoom, deleteRoom, getRoom, getRooms, updatedRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// CREATE=====>>>>>
+router.post("/:hotelId", verifyAdmin, createRoom)
+//UPDATE =====>>>>>
+router.put("/:id",verifyAdmin, updatedRoom)
 
+// DELETE =====>>>>>
+router.delete("/:id",verifyAdmin, deleteRoom)
+
+// GET =====>>>>>
+router.get("/:id", getRoom)
+
+// GET ALL====>>>>>
+router.get("/", getRooms)
 
 export default router
